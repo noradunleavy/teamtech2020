adb316f34ade02727d27f2cf10e86a416fceeb27
ndangarwala <61568916+ndangarwala@users.noreply.github.com>
2020-04-05 12:41:08 -0500

    Revert "yay routing"
    This reverts commit 71ff18fab833916ed4083bd9cbb602411028f70c.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Link, Switch, Route, BrowserRouter } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav'
-import {DataVisualization} from './components/datavis.js';
-import {Settings} from './components/settings.js';
-import {NavigationBar} from './components/NavigationBar.js'
+import DataVisualization from './components/datavis.js';
+import Settings from './components/settings.js';
 
 
   export default class App extends React.Component {
     render() {
       return (
-          <React.Fragment>
-            <Router>
-              <NavigationBar />
+        <div className="App">
+          <BrowserRouter>
+            <div>
+              <Nav>
               <Switch>
-                <Route exact path="/" component={DataVisualization} />
-                <Route path="/settings" component={Settings} />
+                <Route component={DataVisualization} pattern='./components/datavis.js' />
+                <Route component={Settings} pattern='./components/settings.js' />
               </Switch>
-            </Router>
-          </React.Fragment>
+              </Nav>
+            </div>
+          </BrowserRouter>
+        </div>
       );
     }
   }
--- src/components/settings.js
+++ src/components/settings.js
@@ -8,27 +8,7 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-export const Settings = () => (
-    <header className="App-header">
-        <p>
-            Settings Page
-        </p>
-    </header>
-);
-
-/*import styled from 'styled-components';
-const GridWrapper = styled.div`
-
-  display: grid;
-  grid-gap: 10px;
-  margin-top: 1em;
-  margin-left: 6em;
-  margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
-`; */
-
-/* var Link = require('react-router-dom').Link
+var Link = require('react-router-dom').Link
 
 export default class Settings extends React.Component {
   render() {
@@ -46,7 +26,7 @@ export default class Settings extends React.Component {
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Productivity</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/home">Data Visualization</Nav.Link>
+            <Nav.Link href="home">Data Visualization</Nav.Link>
             <Nav.Link href="settings">Settings</Nav.Link>
           </Nav>
         <Form inline>
@@ -63,6 +43,4 @@ export default class Settings extends React.Component {
     </div>
   );
   }
-} */
-
-
+}
