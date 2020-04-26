424a2f7d95eeaba44fce63bf0452b8732081b6b1
ndangarwala <61568916+ndangarwala@users.noreply.github.com>
2020-04-26 16:32:47 -0500

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {useEffect, useState} from "react";
 //import {DataVisualizationTwo} from './components/datavis.js';
 //import Sunburst from 'react-sunburst-d3-v4';
 //import data from './data';
-import {api} from '../api/api.py'
 
 
   export default class App extends React.Component {
@@ -19,9 +18,10 @@ import {api} from '../api/api.py'
             fetch('/framework').then(res => res.json()).then(data=> {
                 setCurrentData(data.request);
             });
-        }, []); */   
+        }, []); 
+              var data = api.get_all_frameworks().data  
+*/   
     render() {
-      var data = api.get_all_frameworks().data  
 
       return (
           <React.Fragment>
@@ -32,7 +32,7 @@ import {api} from '../api/api.py'
                 <Route path="/settings" component={Settings} />
               </Switch>
             </Router>
-             <p>The data is {data}</p> 
+             
 
           </React.Fragment>
       );
