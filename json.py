c7373cdf26753f209117993c81dff1476c42726c
shreyahprasad <sprasa20@illinois.edu>
2020-05-23 17:25:54 -0500

    fixed errors

--- json.py
+++ json.py
@@ -4,13 +4,14 @@ import numpy as np
 # Issue with DataFrame, but should work with data retrieved from MongoDB
 
 def convertToJSON(UUID):
-  # here would be the get dataframe call, parameter UUID
+  #here would be the get dataframe call, parameter UUID
   data = np.array([['5/21 @3:00PM', '5/21 @4:00PM', '5/22 @2:22PM'],
                  ['PRODUCTIVITY', 'SOCIALMEDIA', 'WEATHER'], [-1, 1, 1]])
   data = data.T
   user_frame = pd.DataFrame(data, columns=['TimeStamp', 'Category', 'Type'])
   json = user_frame.to_json(orient='records')
-  print(json)
-  print(user_frame)
+  d_json = {'Anomalies': json}  
+  return d_json  
 
-convertToJSON('eb20b8')
+# for testing
+# print(convertToJSON('eb20b8'))
