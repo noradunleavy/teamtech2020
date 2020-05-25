d089034c30d05231927c82836c588c3df3806b46
Lauren Ho <laurenh4@illinois.edu>
2020-05-24 20:16:03 -0500

    Sunburst toggle

--- frontend/src/components/DataVis/datavis.js
+++ frontend/src/components/DataVis/datavis.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Sunburst from './Sunburst';
 import './datavis.css';
 import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
+import Button from 'react-bootstrap/Button';
 
 import API from '../../api';
 
@@ -13,23 +14,23 @@ export default class DataVisualization extends Component {
       startTimestamp: undefined,
       endTimestamp: undefined,
       sunburstData: null,
-      renderSunburst: false
+      showSunburst: false,
     };
   }
 
-  async componentDidMount() {
-    // Get default sunburst data 
-    let data = await this.getSunburstData(undefined, undefined);
-    this.setState({
-      sunburstData: data,
-    })
-  }
+  // async componentDidMount() {
+  //   // Get default sunburst data 
+  //   let data = await this.getSunburstData(undefined, undefined);
+  //   this.setState({
+  //     sunburstData: data,
+  //   })
+  // }
 
   getSunburstData = async(start, end) => {
     let data = null
     const myAPI = new API({url: 'http://localhost:5000'})
     myAPI.createEntity({ name: 'get'})
-    await myAPI.endpoints.get.sunburstData({uuid: '5ebd070c717f9c1ca90906f41543437a30514f86546931a8acf85f38bf78edbe'}, {start_timestamp: start}, {end_timestamp: end})
+    await myAPI.endpoints.get.sunburstData({uuid: "5ebd070c717f9c1ca90906f41543437a30514f86546931a8acf85f38bf78edbe"}, {start_timestamp: start}, {end_timestamp: end})
       .then(response => data = response.data);
     
     return JSON.parse(JSON.stringify(data));
@@ -39,12 +40,31 @@ export default class DataVisualization extends Component {
     // Convert date into start and end unix timestamps
     let start = Math.floor(date[0].getTime() / 1000)
     let end = Math.floor(date[1].getTime() / 1000)
-
-    let new_sunburst_data = await this.getSunburstData(start, end);
-    this.setState({
+    // TODO: Need to replace with start and end vars
+    let new_sunburst_data = await this.getSunburstData(1512468142, 1512512500);
+    this.setState((prevState) => ({
       date,
-      sunburstData: new_sunburst_data,
+      sunburstData: new_sunburst_data !== "No matches" ? new_sunburst_data : prevState.sunburstData,
+      showSunburst: false,
+    }))
+  }
+
+  toggleSunburst = () => {
+    this.setState({
+      showSunburst: true,
     })
+  }
+
+  displaySunburst = () => {
+    let sunburst = this.state.showSunburst ? <Sunburst data={this.state.sunburstData}
+      width="800" 
+      height="900"           
+      count_member="size"
+      labelFunc={(node)=>node.data.name}
+      _debug={true}
+    /> : null
+    
+    return sunburst
   }
 
   render() {
@@ -54,8 +74,10 @@ export default class DataVisualization extends Component {
           onChange={this.onChangeDateTime}
           value={this.state.date}
           maxDetail = "second"
-          />
-        {this.state.sunburstData !== null && 
+        />
+        <Button onClick={this.toggleSunburst}>View</Button>
+
+        {/* {this.state.sunburstData !== null &&
           <Sunburst data={this.state.sunburstData}
             width="800" 
             height="900"           
@@ -63,7 +85,8 @@ export default class DataVisualization extends Component {
             labelFunc={(node)=>node.data.name}
             _debug={true}
           />
-        }
+        } */}
+        {this.displaySunburst()}
       </div>
     );
   }
