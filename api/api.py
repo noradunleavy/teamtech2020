4e40e824a60b2a28b84a6f12a6095122c383b861
Samantha Walter <sjw2@illinois.edu>
2020-05-27 16:48:05 -0500

    Updated /anomalies/<uuid> to take start and end timestamp query parameters

--- api/api.py
+++ api/api.py
@@ -9,6 +9,7 @@ Author(s):  Samantha Walter <sjw2@illinois.edu>
 """
 
 from time import time
+from datetime import datetime
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -54,9 +55,23 @@ def get_one_sample(uuid):
 
 @app.route('/anomalies/<uuid>')
 def get_anomalies(uuid):
-    response = mongo_db['anomalies'].find_one({'uuid': uuid}, {'_id':0})
-    if response:
-        return response
+    """
+    Returns single document containing matching uuid and list of anomalies with
+    keys for Timestamp, Category, and Type. If query results are empty, returns
+    'No matches'.
+    Takes optional query parameters start and end. If none are given, results
+    between 0 and the current time are returned.
+    Example: /anomalies/5ebd070c717f9c1ca90906f41543437a30514f86546931a8acf85f38bf78edbe?start=1512468142&end=1512500000
+    """
+    # Get query arguments
+    start_timestamp = request.args.get('start')
+    end_timestamp = request.args.get('end')
+
+    pipeline = get_anomalies_pipeline(uuid, start_timestamp, end_timestamp)
+    mongo_cursor = mongo_db['anomalies'].aggregate(pipeline)
+    response_lst = list(mongo_cursor)
+    if response_lst:
+        return response_lst[0]
     else:
         return 'No matches'
 
@@ -81,9 +96,64 @@ def get_sunburst_data():
     else:
         return 'No matches'
 
+def get_anomalies_pipeline(uuid, start_timestamp, end_timestamp):
+    """
+    Builds and returns MongoDB aggregation pipeline to query anomaly data based
+    on uuid and a timestamp range.
+    Expects UNIX timestamps for parameters start_timestamp and end_timestamp
+    """
+    if not start_timestamp:
+        start_timestamp = 0
+    if not end_timestamp:
+        end_timestamp = time()
+    start_datetime = datetime.fromtimestamp(int(start_timestamp))
+    end_datetime = datetime.fromtimestamp(int(end_timestamp))
+
+    # print(start_datetime, datetime.timestamp(start_datetime))
+    # print(end_datetime, datetime.timestamp(end_datetime))
+
+    anomalies_pipeline = [
+        {
+            "$match": {
+                "uuid": uuid
+            }
+        },
+        {
+            "$unwind": {
+                "path": "$anomalies"
+            }
+        },
+        {
+            "$match": {
+                    "anomalies.Timestamp": {
+                    "$gt": start_datetime,
+                    "$lt": end_datetime
+                }
+            }
+        },
+        {
+            "$group": {
+                "_id": "$uuid",
+                "anomalies": { "$push": "$anomalies" }
+            }
+        },
+        {
+            "$set": {
+                "uuid": "$_id"
+            }
+        },
+        {
+            "$project": {
+                "_id": 0
+            }
+        }
+    ]
+    return anomalies_pipeline
+
 def get_sunburst_pipeline(uuid, start_timestamp, end_timestamp):
     """
-    Builds and returns MongoDB aggregation pipeline for sunburst
+    Builds and returns MongoDB aggregation pipeline for sunburst.
+    Expects UNIX timestamps for parameters start_timestamp and end_timestamp
     """
     if not start_timestamp:
         start_timestamp = 0
@@ -91,6 +161,7 @@ def get_sunburst_pipeline(uuid, start_timestamp, end_timestamp):
         end_timestamp = int(time())
     start_timestamp = int(start_timestamp)
     end_timestamp = int(end_timestamp)
+
     sunburst_pipeline = [
         {
             "$match": {
